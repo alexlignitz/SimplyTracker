df9e9d73e976dafe559d8d4e1a2a2d5a23dfd22b
alexlignitz <alexlignitz@gmail.com>
2021-06-06 13:48:05 +0200

    Add main page view

--- SimplyTracker/urls.py
+++ SimplyTracker/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from tracker.views import IndexView
+from tracker.views import IndexView, MainPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', IndexView.as_view(), name="index"),
+    path('main', MainPageView.as_view(), name="main"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 from django.views import View
 
@@ -7,3 +6,8 @@ from django.views import View
 class IndexView(View):
     def get(self, request):
         return render(request, '__base__.html')
+
+
+class MainPageView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, 'main_page.html')
